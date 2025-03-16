d14f045b30dbaed3d85a4a1b272944ff14d4eef0
gihantha <gihantha01@gmail.com>
2025-03-16 09:31:30 +0530

    project initializing

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import TestComponent from "./components/TestComponent.jsx";
+
+function App() {
+
+
+  return (
+    <>
+        <TestComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TestComponent.jsx
@@ -0,0 +1,10 @@
+
+const TestComponent = () => {
+    return (
+        <div>
+            <h1>huhS</h1>
+        </div>
+    );
+};
+
+export default TestComponent;
