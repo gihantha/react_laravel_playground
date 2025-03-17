084796fa7ccc6224d29998557ac3b225e6e5c44d
gihantha <gihantha01@gmail.com>
2025-03-17 22:49:39 +0530

    useEffect

--- src/components/TestComponent.jsx
+++ src/components/TestComponent.jsx
@@ -1,35 +1,27 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 
 const TestComponent = () => {
-    const [count, setCount] = useState(0); 
-    console.log(count);
-    const handleIncrease = () => {
-        setCount(count + 1)
-        return count
-          
-    }
+    const [count, setCount] = useState(0)
 
-    const handleDecrease = () => {
-        setCount(count - 1)
-    }
+    useEffect(() => {
+        return () => {
+            console.log("Component unmounted");
+        }
+    }, [count]);
+
 
     return (
         <div>
-            <h1>count is {count}</h1>
-            <button 
-                type="button" 
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                onClick={handleIncrease}
+            <button
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium
+                rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none
+                dark:focus:ring-blue-800"
+                onClick={() => setCount(count + 1)}
                 >
                 +
             </button>
-            <button 
-                type="button" 
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                onClick={handleDecrease}
-                >
-                -
-            </button>
+
         </div>
     );
 };
