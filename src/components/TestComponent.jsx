6b241860d4e04aeeb042fdb53f4e1f47cc9876c2
gihantha <gihantha01@gmail.com>
2025-03-17 13:40:08 +0530

    useState use in increment and decrement

--- src/components/TestComponent.jsx
+++ src/components/TestComponent.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
 
 const TestComponent = () => {
+    const [count, setCount] = useState(0); 
+    console.log(count);
+    const handleIncrease = () => {
+        setCount(count + 1)
+        return count
+          
+    }
+
+    const handleDecrease = () => {
+        setCount(count - 1)
+    }
+
     return (
         <div>
-            <h1>huhS</h1>
+            <h1>count is {count}</h1>
+            <button 
+                type="button" 
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                onClick={handleIncrease}
+                >
+                +
+            </button>
+            <button 
+                type="button" 
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                onClick={handleDecrease}
+                >
+                -
+            </button>
         </div>
     );
 };
